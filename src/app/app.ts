fc0e68333ffce1a584ed0dd6e0b2b7b65710ea05
Jihad <Jmahdad2110@outlook.com>
2025-11-28 11:54:59 +0100

    Initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListProduit } from './list-produit/list-produit';
+import { Auth } from './auth/auth';
+import { Categories } from './categories/categories';
+import { Footer } from './footer/footer';
+import { SearchBar } from './search-bar/search-bar';
+import { Header } from './header/header';
+import { Panier } from './panier/panier';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ListProduit, Auth, Categories, Footer, SearchBar, Header, Panier, RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('nom-projet');
