4e06297bc37941bca5c4a47dd5be7b81640399a7
Jihad <Jmahdad2110@outlook.com>
2025-12-25 19:56:50 +0100

    v2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { ListProduit } from './list-produit/list-produit';
 import { Auth } from './auth/auth';
 import { Categories } from './categories/categories';
@@ -11,7 +12,7 @@ import { CategorySidebarComponent } from './category-sidebar/category-sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [ListProduit, Auth, Categories, Footer, SearchBar, Header, PanierComponent, CategorySidebarComponent, RouterOutlet],
+  imports: [ListProduit, Auth, Categories, Footer, SearchBar, Header, PanierComponent, CategorySidebarComponent, RouterOutlet, RouterModule],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
